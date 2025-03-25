255cec029932a3bc66b40736d8af9964433c6051
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2025-03-25 12:52:58 +0000

    Add folders to views and styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
-import ProductsPage from "./pages/products-page";
-import ProductDetailPage from "./pages/product-details";
-import AddProductPage from "./pages/add-products";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import ProductsPage from "./pages/products/products-page";
+import ProductDetailPage from "./pages/products/product-details";
+import AddProductPage from "./pages/products/add-products";
+import Login from "./pages/account/login";
+import Register from "./pages/account/register";
 import ErrorPage from "./pages/error-pages/login-error";
-import Profile from "./pages/profile";
+import Profile from "./pages/profile/profile";
 import HomePage from "./pages/home";
 import AddProductsError from "./pages/error-pages/access-control-error";
-import AdminPanel from "./pages/admin-panel";
+import AdminPanel from "./pages/profile/admin-panel";
 import "./App.css";
 
 const App = () => {
--- src/pages/products/product-details.js
+++ src/pages/products/product-details.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "../styling/product-details.css";
+import "../../styling/products/product-details.css";
 
 const ProductDetailPage = () => {
   const { id } = useParams();
